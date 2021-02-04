e05bcb350a4c7a0b55b669fb001a7ab224a8dd76
Q. Soerokarso <quincysoeroakrso@novi-education.nl>
2021-02-04 21:55:56 +0100

    InvoiceResponse

---
+++ src/main/java/com/quincy/database_test/payload/response/InvoiceResponse.java
@@ -0,0 +1,31 @@
+package com.quincy.database_test.payload.response;
+
+public class InvoiceResponse {
+    private Long invoiceId;
+    private Long productId;
+    private Long quantity;
+
+    public Long getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(Long invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+}
